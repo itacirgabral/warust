262b3e783f09c4b8d6cc1e1e4e6d2ee69eed5494
Itacir Gabral <itacirgabral@gmail.com>
2020-07-01 22:25:00 -0400

    code debug does last pops - no!

--- src/unique.rs
+++ src/unique.rs
@@ -3,7 +3,31 @@ where
     T: std::iter::IntoIterator,
     T::Item: std::cmp::PartialEq + std::fmt::Debug,
 {
-    vec![]
+    /*
+    let mut eh_primeira = true;
+    let mut unique: Vec<T::Item> = Vec::new();
+
+    for s in sequence.into_iter() {
+        if eh_primeira {
+            println!("ehPrimeiro");
+            eh_primeira = false;
+            unique.push(s);
+        } else {
+
+        };
+    };
+    Vec::new()
+    */
+    let v = [10, 40, 30];
+    match v.last() {
+        None => println!("None"),
+        Some(lst) => {
+            println!("last {}", lst);
+        },
+    };
+    println!("{:#?}", v);
+    let unique: Vec<T::Item> = Vec::new();
+    unique
 }
 
 #[test]
