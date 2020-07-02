a2fb2149b14d8c0a6d068ce8e334d2f2e9aa9f21
Itacir Gabral <itacirgabral@gmail.com>
2020-07-01 22:37:35 -0400

    unique

--- src/unique.rs
+++ src/unique.rs
@@ -3,30 +3,17 @@ where
     T: std::iter::IntoIterator,
     T::Item: std::cmp::PartialEq + std::fmt::Debug,
 {
-    /*
-    let mut eh_primeira = true;
     let mut unique: Vec<T::Item> = Vec::new();
-
     for s in sequence.into_iter() {
-        if eh_primeira {
-            println!("ehPrimeiro");
-            eh_primeira = false;
-            unique.push(s);
-        } else {
-
-        };
-    };
-    Vec::new()
-    */
-    let v = [10, 40, 30];
-    match v.last() {
-        None => println!("None"),
-        Some(lst) => {
-            println!("last {}", lst);
-        },
-    };
-    println!("{:#?}", v);
-    let unique: Vec<T::Item> = Vec::new();
+        match unique.last() {
+            None => unique.push(s),
+            Some(lst) => {
+                if s != *lst {
+                    unique.push(s);
+                }
+            },
+        }
+    }
     unique
 }
 
